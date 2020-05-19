ae5d0176816cca00e533f7863bf2a0a6fcd3af96
Jessica Manent <jessica.manent@gmail.com>
2020-05-19 12:45:54 +0200

    Remove the hardcoded comments

--- models/PDO.php
+++ models/PDO.php
@@ -1,10 +1,10 @@
 <?php
-$db_user = "dbuser";
-$db_passwd = "1234Soleil!";
+$db_user = "jessica";
+$db_passwd = "toupi25juillet!01AVRIL";
 $db_host = "localhost";
 $db_port = "3306";
 $db_name = "tsn";
 $db_dataSourceName = "mysql:host=$db_host;port=$db_port;dbname=$db_name";
 
-$PDO = new PDO($db_dataSourceName, $db_user, $db_passwd);
-$PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$PDO = new PDO($db_dataSourceName, $db_user, $db_passwd); // L'objet PDO a besoin de ces trois paramètres
+$PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Permet d'afficher les erreurs, à enlever en prod
